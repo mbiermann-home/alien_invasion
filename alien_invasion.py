e6278598c38df841121086725b69fc0abf7906cc
mbiermann-home <fertilesound@yahoo.com>
2021-01-30 10:32:49 -0600

    starting code

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,1 +1,11 @@
-# begin
+import sys
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
