dc1544dd82cf82042a7fe8360049a3db9ca6e61a
mbiermann-home <fertilesound@yahoo.com>
2021-02-01 06:54:53 -0600

    Bullets!

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -12,7 +13,6 @@ class AlienInvasion:
         self.settings = Settings()
 
         # Sets the size of the screen
-        
         # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         # Sets the screen to fullsize
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
@@ -21,6 +21,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         # Set the background color
         self.bg_color = (230, 230, 230)
@@ -30,6 +31,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -46,6 +48,8 @@ class AlienInvasion:
         """Updates images on the screen and flips to the new screen"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
@@ -58,6 +62,8 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             # Move the ship to the left
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
             # Exit the game
             sys.exit()
@@ -70,6 +76,11 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             # Move the ship to the left
             self.ship.moving_left = False
+    
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
