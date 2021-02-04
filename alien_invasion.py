51e4a5517b66e238608c85d1a043e07bd5fb54de
mbiermann-home <fertilesound@yahoo.com>
2021-02-04 09:14:13 -0600

    Added collisions

--- alien_invasion.py
+++ alien_invasion.py
@@ -143,7 +143,7 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
-        #Check for any bullets that have hit aliens
+        #Check for any bullets that have hit aliens 
         #   If so, get rid of the bullet and the alien
         collisions = pygame.sprite.groupcollide(
                     self.bullets, self.aliens, True, True)
